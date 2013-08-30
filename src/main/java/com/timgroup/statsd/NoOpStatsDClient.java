6c60be3bc03e54fc76853d12066fcd0800d8abd2
Scott Parkerson <scott@parkerson.net>
2013-08-29 23:40:01 -0400

    More @Override cleanup

--- src/main/java/com/timgroup/statsd/NoOpStatsDClient.java
+++ src/main/java/com/timgroup/statsd/NoOpStatsDClient.java
@@ -8,14 +8,14 @@ package com.timgroup.statsd;
  *
  */
 public final class NoOpStatsDClient implements StatsDClient {
-    @Override public void stop() { }
-    @Override public void count(String aspect, int delta) { }
-    @Override public void incrementCounter(String aspect) { }
-    @Override public void increment(String aspect) { }
-    @Override public void decrementCounter(String aspect) { }
-    @Override public void decrement(String aspect) { }
-    @Override public void recordGaugeValue(String aspect, int value) { }
-    @Override public void gauge(String aspect, int value) { }
-    @Override public void recordExecutionTime(String aspect, int timeInMs) { }
-    @Override public void time(String aspect, int value) { }
+    public void stop() { }
+    public void count(String aspect, int delta) { }
+    public void incrementCounter(String aspect) { }
+    public void increment(String aspect) { }
+    public void decrementCounter(String aspect) { }
+    public void decrement(String aspect) { }
+    public void recordGaugeValue(String aspect, int value) { }
+    public void gauge(String aspect, int value) { }
+    public void recordExecutionTime(String aspect, int timeInMs) { }
+    public void time(String aspect, int value) { }
 }
